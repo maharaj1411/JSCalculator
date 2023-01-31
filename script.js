0be4f64be8509a72e36600c41d9552a870e5b1e5
maharaj1411 <raazmax@gmail.com>
2023-01-31 23:02:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function display(value)
+{
+    document.getElementById('textarea').value += value;
+}
+
+function calculate() {
+    let value = document.getElementById('textarea').value;
+    //console.log(value);
+
+    let answer = eval(value);
+
+    document.getElementById('textarea').value = answer;
+}
+
+function clr() {
+    document.getElementById('textarea').value = '';
+}
